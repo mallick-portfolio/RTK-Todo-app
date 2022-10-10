6bb13db42775f7c7e1145829868f998ec151c125
mallick-portfolioE <tamal1714019@gmail.com>
2022-10-10 16:18:00 +0600

    todo api ready

--- rtk-server/route/todoHandler.js
+++ rtk-server/route/todoHandler.js
@@ -4,14 +4,67 @@ const route = express.Router();
 const Todo = require("../model/todoSchema.js");
 
 route.get("/", async (req, res) => {
-  const result = await Todo.find({});
-  console.log(result);
-  res.status(200).json({
-    message: "Data Successfull",
-    data: result,
-  });
+  try {
+    const result = await Todo.find({});
+    res.status(200).json({
+      message: "Data Successfull",
+      data: result,
+    });
+  } catch {
+    res.status(404).json({
+      error: "Data not found",
+    });
+  }
 });
-route.post("/", async (req, res) => {});
-route.put("/:id", async (req, res) => {});
-route.delete("/:id", async (req, res) => {});
+
+route.post("/", async (req, res) => {
+  try {
+    const newTodo = new Todo(req.body);
+    await newTodo.save();
+    res.status(500).json({
+      message: "Todo Added Successfull",
+    });
+  } catch {
+    res.status(200).json({
+      message: "Todo Failed to added",
+    });
+  }
+});
+
+route.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const filter = { _id: id };
+    const body = req.body;
+    let result = await Todo.findOneAndUpdate(filter, body);
+    res.status(201).json({
+      data: result,
+      message: "updated successfull",
+    });
+  } catch {
+    res.status(204).json({
+      message: "Failed to update",
+    });
+  }
+});
+route.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Todo.deleteOne({ _id: id });
+    res.send(result);
+    if (result.acknowledged) {
+      res.status(202).json({
+        message: "Delete Successfull",
+      });
+    } else {
+      res.status(204).json({
+        error: "Failed to delete",
+      });
+    }
+  } catch {
+    res.status(204).json({
+      error: "Failed to delete",
+    });
+  }
+});
 module.exports = route;
